ed0a2656de90bafd080096eef57bd1bd3818007f
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-04-23 12:43:32 -0400

    finished request 4 and step 1 callbacks

--- index.js
+++ index.js
@@ -105,10 +105,14 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(arr){
+    const totalPop = zooAnimals.reduce(function(acc, item){
+      return acc + item.population;
+    }, 0);
+    return totalPop;
   }
   
+  console.log('request 4', USApop(zooAnimals));
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
   /* 🦁🦁🦁 Step 1: Create a higher-order function 🦁🦁🦁
@@ -118,8 +122,8 @@ const zooAnimals = [
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
+  function consume(a, b, cb){
+    return cb(a, b);
   }
  
   
